3e833639db31d71f703665de4a841fe2bef2fa05
user404e <e404user@gmail.com>
2023-05-15 12:50:32 +0000

    JAVA

--- 11 ( METHOD - 1 )/SortDescending.java
+++ 11 ( METHOD - 1 )/SortDescending.java
@@ -3,7 +3,9 @@ import java.util.Arrays;
 public class SortDescending {
     public static void main(String[] args) {
          int[] a1 = {1,2,4,43,4,234,23};
+
          Arrays.sort(a1);
+
          int[] a2 = new int[a1.length];
          int length = a1.length-1;
 
@@ -11,10 +13,9 @@ public class SortDescending {
              a2[i] = a1[length];
              length--;
          }
+
          for(int i=0;i<a2.length;i++){
             System.out.println(a2[i]);
-         }
-        
-        
+         }    
     }
 }
---
+++ 12/MathClass.java
@@ -0,0 +1,8 @@
+import java.math.*;
+
+public class MathClass {
+    public static void main(String[] args) {
+       int num1 = -23;
+       System.out.println("ABSOLUTE = " + Math.abs(num1)); 
+    }
+}
