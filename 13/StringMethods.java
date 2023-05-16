87244bcb760178fee9df5cd0cf78d3c1c635da9d
user404e <e404user@gmail.com>
2023-05-16 08:25:48 +0000

    JAVA

--- 13/StringMethods.java
+++ 13/StringMethods.java
@@ -4,21 +4,21 @@ class StringMethods {
         String s2 = "HELLO";
         String s3 = "Alex,Harry,Brian,Zex";
         String[] names;
-        System.out.println("LENGTH = " + s1.length());
-        System.out.println(s1.equals(s2));
+        System.out.println("LENGTH = " + s1.length()); // length method
+        System.out.println(s1.equals(s2)); // equals method
 
-        names = s3.split(",");
+        names = s3.split(","); // split method
         for(String name:names){
             System.out.print(name + " ");
         }
 
-        System.out.println(s1.compareTo(s2));
-        System.out.println(s1.concat(s3));
+        System.out.println(s1.compareTo(s2)); // comapreTo method
+        System.out.println(s1.concat(s3)); // concat method
 
-        System.out.println(s2.toLowerCase());
-        System.out.println(s1.toUpperCase());
+        System.out.println(s2.toLowerCase()); // tolowerCase method
+        System.out.println(s1.toUpperCase()); // toUpperCase method
 
-        System.out.println(s3.substring(5,10));
+        System.out.println(s3.substring(5,10)); // substring method
 
     }
 }
